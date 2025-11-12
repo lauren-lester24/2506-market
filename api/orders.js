3b61138d15ce4b0db651e93e32428f0cf75cfbe7
lauren-lester24 <lauren.lester24@yahoo.com>
2025-11-11 23:06:02 -0600

    completed first section of post orders

--- api/orders.js
+++ api/orders.js
@@ -1,8 +1,32 @@
 import express from "express";
+import requireUser from "#middleware/requireUser";
+import requireBody from "#middleware/requireBody";
 
 const router = express.Router();
 
+import { createOrder } from "#db/queries/orders";
 
+router.post("/",
+    requireUser,
+    requireBody(["date"]),
+    async (req, res) => {
+        try {
+            const { date, note } = req.body;
+         
+    
+            const newOrder = await createOrder({
+                date,
+                note,
+           userId: req.user.id
+            });
+
+            res.status(201).send(newOrder);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+)
 
 
 
--- db/queries/orders.js
+++ db/queries/orders.js
@@ -1,15 +1,16 @@
 import db from "#db/client";
 
-export async function createOrder(userId, date, note) {
+export async function createOrder({ date, note, userId }) {
 const sql = `
 INSERT INTO orders
-(user_id, date, note)
+(date, note, user_id)
 VALUES
 ($1, $2, $3)
 RETURNING *
 `;
 const {
     rows: [order],
-} = await db.query(sql, [userId, date, note]);
+} = await db.query(sql, [date, note, userId]);
 return order;
 }
+
